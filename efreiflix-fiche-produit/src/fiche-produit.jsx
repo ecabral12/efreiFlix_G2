4ca6b55325402fc60505b00415b9db3e26ccb47b
Elvis Cabral <ecabral@dotscreen.com>
2025-02-13 12:51:40 +0100

    pareil

--- efreiflix-fiche-produit/src/fiche-produit.jsx
+++ efreiflix-fiche-produit/src/fiche-produit.jsx
@@ -33,44 +33,31 @@ const MovieDetail = () => {
   }
 
   return (
-    <div className="movie-detail-container">
-      <div className="movie-detail">
-        <div className="movie-poster">
-          <img src={movie.posterUrl} alt={movie.title} />
-        </div>
-
-        <div className="movie-info">
-          <h1 className="movie-title">{movie.title} ({movie.year})</h1>
-          <div className="movie-genres">
-            {movie.genres.map((genre, index) => (
-              <span key={index} className="genre">{genre}</span>
-            ))}
-          </div>
-          <div className="movie-rating">
-            <strong>Rating:</strong> {movie.rating} / 5
-          </div>
-
-          <p className="movie-description">{movie.description}</p>
-
-          <div className="movie-buttons">
-            <button className="play-button">► Play</button>
-            <button className="trailer-button">Trailer</button>
-          </div>
-
-          <div className="movie-trailer">
-            <iframe
-              width="100%"
-              height="400"
-              src={movie.trailerUrl}
-              title={`${movie.title} Trailer`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </div>
-      </div>
+    <div className="movie-card" data-movie={title}>
+    <div className="movie-cardoverlay"></div>
+    <div className="movie-cardshare">
+      <button className="movie-cardicon">
+        <i className="material-icons">&#xe87d;</i>
+      </button>
+      <button className="movie-cardicon">
+        <i className="material-icons">&#xe253;</i>
+      </button>
+      <button className="movie-cardicon">
+        <i className="material-icons">&#xe80d;</i>
+      </button>
     </div>
+    <div className="movie-cardcontent">
+      <div className="movie-cardheader">
+        <h1 className="movie-cardtitle">{title}</h1>
+        <h4 className="movie-cardinfo">{info}</h4>
+      </div>
+      <p className="movie-carddesc">{description}</p>
+      <button className="btn btn-outline movie-card__button" type="button">
+        Watch Trailer
+      </button>
+    </div>
+  </div>
+
   );
 };
 
